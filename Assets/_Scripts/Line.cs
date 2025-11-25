9a9f9d37a97dd94aa827db0fef72c224d4373ad0
abhishekm-cell <abhishekm@alantrixgeim.com>
2025-11-25 15:56:24 +0530

    line draw mech
    line draw mech with collider finished

---
+++ Assets/_Scripts/Line.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] private LineRenderer _renderer;
+    [SerializeField] private EdgeCollider2D _collider;
+    private List<Vector2> points = new List<Vector2>();
+
+    void Awake()
+    {
+        _collider.transform.position -= transform.position;
+    }
+
+
+
+    public void SetPosition(Vector2 pos)
+    {
+        if(!CanAppend(pos))
+        {
+            return;
+        }
+
+        points.Add(pos);
+
+        _renderer.positionCount++;
+        _renderer.SetPosition(_renderer.positionCount - 1, pos);
+
+
+        _collider.points = points.ToArray(); 
+    } 
+
+    private bool CanAppend(Vector2 pos)
+    {
+        if(_renderer.positionCount == 0)
+        {
+            return true;
+        }
+        return Vector2.Distance(_renderer.GetPosition(_renderer.positionCount - 1), pos) > DrawManager.Resolution; // _renderer.positionCount - 1 = last pos
+    }
+
+
+
+}
