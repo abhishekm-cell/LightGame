f1fae8101c4c7d9fc3698582f765ede692782a2c
abhishekm-cell <abhishekm@alantrixgeim.com>
2025-11-25 18:25:12 +0530

    touch input added

---
+++ Assets/_Scripts/Game/DrawManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawManager : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private Line linePrefab;
+    private Line _currentLine;
+    public const float Resolution = 0.15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleDraw();
+    }
+
+    // void HandleDraw()
+    // {
+    //     Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    //     if (Input.GetMouseButtonDown(0) || Input.GetTouch(0).phase == TouchPhase.Began)
+    //     {
+    //         _currentLine = Instantiate(linePrefab, mousePos, Quaternion.identity);
+            
+    //     }
+    //     if(Input.GetMouseButton(0) || Input.GetTouch(0).phase == TouchPhase.Began)
+    //     {
+    //         _currentLine.SetPosition(mousePos);
+    //     }
+    // }
+
+    void HandleDraw()
+    {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        // Start line on mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentLine = Instantiate(linePrefab, mousePos, Quaternion.identity);
+        }
+
+        // Continue drawing while mouse held
+        if (Input.GetMouseButton(0))
+        {
+            _currentLine?.SetPosition(mousePos);
+        }
+
+        // TOUCH SUPPORT
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = cam.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                _currentLine = Instantiate(linePrefab, touchPos, Quaternion.identity);
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                _currentLine?.SetPosition(touchPos);
+            }
+        }
+    }
+}
